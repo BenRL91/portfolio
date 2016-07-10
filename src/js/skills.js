60f1f10cc9725c6d98e580411131fec150ad3763
Ben Lee <leeohsunshine@gmail.com>
2016-07-10 11:05:56 -0400

    Add icons to navbar, add skills chart component.

--- src/js/skills.js
+++ src/js/skills.js
@@ -1,18 +1,70 @@
 import React, { Component, PropTypes } from 'react';
+import skills from './skillsData';
 
+let that;
 export default class Skills extends Component {
 
- chartSkill(skill){
+constructor( ...args ){
+  super( ...args );
+  that = this;
+  that.state = {
+     loaded: false,
+     sortedSkills: skills.sort(that.compare)
+  }
+}
+compare( skill_one, skill_two ) {
+ if ( skill_one.height > skill_two.height ) {
+   return -1;
+ }
+ if ( skill_one.height < skill_two.height ) {
+   return 1;
+ }
+ return 0;
+}
+
+ chartSkill( skill ){
+   let x = 0;
+
+   let splitSkill = skill.skill.split( ' ' );
+
+   function oneLetterToALine( word ){
+    return <span key={ ++x }>{ word }</span>
+   }
    return (
-     <div className='skill' style={`height: ${skill.height}`}></div>
+     <div key = { skill.id }
+          className={ `skill ${ skill.skill }` }
+          style={ {
+            height: that.state.loaded
+                    ? `${skill.height}%`
+                    : '0%',
+            // backgroundColor: skill.id % 2 === 0 ? 'grey' : 'white'
+            backgroundColor: skill.backgroundColor
+          } }>
+          { splitSkill.map( oneLetterToALine ) }
+     </div>
    )
  }
+  revealChart(){
 
+   that.timeoutID = setTimeout( function(){
+
+     that.setState( {
+       loaded: true
+     } )
+
+   }, 500 )
+
+ }
+  componentDidMount(){
+    that.revealChart();
+  }
   render() {
+    let { sortedSkills } = that.state;
+    console.log('i am rendering')
     return (
       <div className='skills-wrapper'>
-        <div className='skillChart'>
-          { SKILLS.map( this.chartSkill ) }
+        <div className='skill-chart'>
+          { sortedSkills.map( this.chartSkill ) }
         </div>
       </div>
     )
--- src/js/skillsData.js
+++ src/js/skillsData.js
@@ -1,5 +1,65 @@
-const SKILLS = {
+let skills = [
+  {
+    skill: 'JavaScript',
+    height: 90,
+    backgroundColor: 'red',
+    id: 0
+  },
+  {
+    skill: 'React.js',
+    height: 87,
+    backgroundColor: 'orange',
+    id: 1
+  },
+  {
+    skill: 'jQuery',
+    height: 61,
+    backgroundColor: 'gray',
+    id: 8
+  },
+  {
+    skill: 'Git/ Github',
+    height: 70,
+    backgroundColor: 'yellow',
+    id: 2
+  },
+  {
+    skill: 'HTML5',
+    height: 77,
+    backgroundColor: 'purple',
+    id: 4
+  },
+  {
+    skill: 'SASS/ CSS',
+    height: 86,
+    backgroundColor: 'blue',
+    id: 5
+  },
+  {
+    skill: 'Angular',
+    height: 40,
+    backgroundColor: 'indigo',
+    id: 6
+  },
+  {
+    skill: 'node.js',
+    height: 42,
+    backgroundColor: 'lightgreen',
+    id: 7
+  },
+  {
+    skill: 'Command Line',
+    height: 64,
+    backgroundColor: 'green',
+    id: 3
+  },
+  {
+    skill: 'ES2015/ Stage-0',
+    height: 75,
+    backgroundColor: 'white',
+    id: 9
+  }
 
-}
+]
 
-export SKILLS;
+export default skills;
